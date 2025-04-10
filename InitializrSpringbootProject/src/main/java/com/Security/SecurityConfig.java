740c64d5df5fead49d520d8b7b37f6e3a4b62f6e
Chara <164945066+CharaDr39@users.noreply.github.com>
2025-04-09 20:37:06 -0600

    creacion de la mayoria de carpetas

---
+++ InitializrSpringbootProject/src/main/java/com/Security/SecurityConfig.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.Security;
+
+/**
+ *
+ * @author chara
+ */
+public class SecurityConfig {
+
+}
